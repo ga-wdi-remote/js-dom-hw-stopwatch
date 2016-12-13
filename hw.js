3f4408b61b2048c78bfef58bdf6807bbfce84e9d
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-12-13 00:49:00 -0500

    Make service objects 'global'
    For testing purposes; that way, Mocha can run tests on their methods.

--- hw.js
+++ hw.js
@@ -2,61 +2,62 @@
 // Copyright (C) 2016 Matt Brendzel under the GNU General Public License.
 // See LICENSE for details.
 
+/// Application Logic ///
+const StopwatchLogic = {
+  advanceClock: function(){ // Provided Code : DO NOT EDIT THIS FUNCTION
+    if (Stopwatch.isRunning) {
+      Stopwatch.updateTimeValues();
+      setTimeout(Stopwatch.advanceClock, 10);
+    }
+  },
+  isRunning: false,
+  mins: 0,
+  secs: 0,
+  millisecs: 0,
+  laps: [],
+  updateTimeValues: function(){
+    // Your Code Here
+  },
+  reset: function(){
+    // Your Code Here
+  },
+  start: function(){
+    // Your Code Here
+  },
+  stop: function(){
+    // Your Code Here
+  },
+  lap: function(){
+    // Your Code Here
+  }
+};
+
+/// User Interface ///
+const Display = {
+  zeroFill: function(number, length){
+    // Your Code Here
+  },
+  updateTimeDisplay: function(){
+    // Your Code Here
+  },
+  updateLapList: function(){
+    // Your Code Here
+  },
+};
+const EventHandlers = {
+  onClickStart: function() {
+    // Your Code Here
+  },
+  onClickLap: function(){
+    // Your Code Here
+  },
+  onClickStopReset: function(){
+    // Your Code Here
+  }
+};
+
+
 window.onload = function(){
-  /// DO NOT EDIT ABOVE THIS LINE ///
-
-  /// Application Logic ///
-  var Stopwatch = {
-    advanceTheClock: function(){ // Provided Code : DO NOT EDIT THIS FUNCTION
-      if (Stopwatch.isRunning) {
-        Stopwatch.updateTimeValues();
-        setTimeout(Stopwatch.advanceTheClock, 10);
-      }
-    },
-    isRunning: false,
-    mins: 0,
-    secs: 0,
-    millisecs: 0,
-    laps: [],
-    updateTimeValues: function(){
-      // Your Code
-    },
-    reset: function(){
-      // Your Code Here
-    },
-    start: function(){
-      // Your Code Here
-    },
-    stop: function(){
-      // Your Code Here
-    },
-    lap: function(){
-      // Your Code Here
-    }
-  };
-
-  /// User Interface ///
-  var Display = {
-    zeroFill: function(number, length){
-      // Your Code Here
-    },
-    updateTimeDisplay: function(){
-      // Your Code Here
-    },
-    updateLapList: function(){
-      // Your Code Here
-    },
-  };
-  var EventHandlers = {
-    onClickStart: function() {
-      // Your Code Here
-    },
-    onClickLap: function(){
-      // Your Code Here
-    },
-    onClickStopReset: function(){
-      // Your Code Here
-    }
-  };
   // Set your event handlers in the lines below.
+
 };
